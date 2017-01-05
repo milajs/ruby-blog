cb38638601d84b4d582d1e50e5a7e493894edff0
Lyudmila Promyslova <lpromyslova@mail.ru>
2017-01-06 00:05:46 +0300

    new: add edit and delete methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
 
@@ -19,6 +23,23 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
